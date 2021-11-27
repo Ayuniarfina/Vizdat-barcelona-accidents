aa6f3360c5bb170478fd5a334272fb0b042dc2b3
JosuaCrishan <94295909+JosuaCrishan@users.noreply.github.com>
2021-11-27 17:35:51 +0700

    Update callbacks.py

--- callbacks.py
+++ callbacks.py
@@ -6,8 +6,8 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 # Import dash html, bootstrap components, and tables for datatables
-# import dash_html_components as html
-# import dash_bootstrap_components as dbc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
 # import dash_core_components as dcc
 # import dash_table
 
@@ -15,6 +15,8 @@ from plotly.subplots import make_subplots
 from app import app
 import pandas as pd
 import pathlib as pl
+import plotly.express as px
+import json
 
 
 # Import custom data.py
@@ -25,6 +27,14 @@ DATA_PATH = PATH.joinpath("data").resolve()
 
 df = pd.read_csv(DATA_PATH.joinpath("accidents_2017.csv"))
 df = d.praprocess(df)
+
+data = pd.read_csv("new_accidents_2017.csv")
+data["Date"] = pd.to_datetime(data["Date"], format="%Y-%m-%d")
+data.sort_values("Date", inplace=True)
+df_mild = pd.read_csv(DATA_PATH.joinpath("sum_mild_injuries.csv"))
+
+with open('districtes.geojson') as json_data:
+    Barcelona_data = json.load(json_data)
 
 @app.callback(Output('my-graph', 'figure'), [Input('filter-month', 'value')])
 def update_graph(selected_dropdown_value):
@@ -57,3 +67,125 @@ def r_trend_month(df_):
         # 'layout': {'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30}
         
     }
+
+@app.callback(
+    output=[Output("plot-mild","figure"), 
+    Output("plot-serious","figure"),
+    Output("output-panel","children")], 
+
+    inputs=[Input("district","value"), 
+    Input("date-range","start_date"), 
+    Input("date-range","end_date")]
+) 
+def plot_mild_cases(district, start_date, end_date):
+    if district == 'All District':
+        df_filter = data.loc[((data['Date'] >= start_date) & (data['Date'] <= end_date))]
+        #Mild
+        accident_cnt_per_district_mild = df_filter.groupby(by="District_Name").sum()[["Mild_injuries", "Victims", "Vehicles_involved"]]\
+                                .rename(columns={"Mild_injuries":"Count_Mild_injuries"})
+        accident_avg_lat_lon = df_filter.groupby(by="District_Name").mean()[["Longitude", "Latitude"]]
+
+        accident_cnt_per_district_mild = accident_cnt_per_district_mild.join(accident_avg_lat_lon)
+        accident_cnt_per_district_mild = accident_cnt_per_district_mild.reset_index()
+
+        ## mild plots
+        fig1 = px.choropleth_mapbox(accident_cnt_per_district_mild, 
+                            geojson=Barcelona_data, 
+                            color="Count_Mild_injuries",
+                            locations="District_Name", 
+                            featureidkey="properties.NOM",
+                            center={"lat": 41.389223, "lon": 2.167939},
+                            mapbox_style="open-street-map", 
+                            color_continuous_scale='sunsetdark',
+                            zoom=10) 
+        fig1.update_layout(margin={"r":0,"t":0,"l":0,"b":0})   
+
+        #Serious
+        accident_cnt_per_district_serious = df_filter.groupby(by="District_Name").sum()[["Serious_injuries", "Victims", "Vehicles_involved"]]\
+                                    .rename(columns={"Serious_injuries":"Count_Serious_injuries"})
+        accident_avg_lat_lon = df_filter.groupby(by="District_Name").mean()[["Longitude", "Latitude"]]
+
+        accident_cnt_per_district_serious = accident_cnt_per_district_serious.join(accident_avg_lat_lon)
+        accident_cnt_per_district_serious = accident_cnt_per_district_serious.reset_index()
+
+        ## main plots
+        fig2 = px.choropleth_mapbox(accident_cnt_per_district_serious, 
+                            geojson=Barcelona_data, 
+                            color="Count_Serious_injuries",
+                            locations="District_Name", 
+                            featureidkey="properties.NOM",
+                            center={"lat": 41.389223, "lon": 2.167939},
+                            mapbox_style="open-street-map", 
+                            color_continuous_scale='sunsetdark',
+                            zoom=10)
+        ## set background color
+        fig2.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+        panel = html.Div([
+            html.H4(district),
+            dbc.Card(body=True, className="text-white bg-primary", children=[
+                
+                html.H6("Total Victims:", style={"color":"white"}),
+                html.H3("{:,.0f}".format(accident_cnt_per_district_mild['Victims'].sum()), style={"color":"white"}),
+                
+                html.H6("Total Vehicles Involved:", className="text-danger"),
+                html.H3("{:,.0f}".format(accident_cnt_per_district_mild['Vehicles_involved'].sum()), className="text-danger")
+            ])
+        ])
+    else:
+        df_filter = data.loc[(data['District_Name'] == district) & ((data['Date'] >= start_date) & (data['Date'] <= end_date))]
+
+        #Mild
+        accident_cnt_per_district_mild = df_filter.groupby(by="District_Name").sum()[["Mild_injuries", "Victims", "Vehicles_involved"]]\
+                                .rename(columns={"Mild_injuries":"Count_Mild_injuries"})
+        accident_avg_lat_lon = df_filter.groupby(by="District_Name").mean()[["Longitude", "Latitude"]]
+
+        accident_cnt_per_district_mild = accident_cnt_per_district_mild.join(accident_avg_lat_lon)
+        accident_cnt_per_district_mild = accident_cnt_per_district_mild.reset_index()
+
+        ## mild plots
+        fig1 = px.choropleth_mapbox(accident_cnt_per_district_mild, 
+                            geojson=Barcelona_data, 
+                            color="Count_Mild_injuries",
+                            locations="District_Name", 
+                            featureidkey="properties.NOM",
+                            center={"lat": 41.389223, "lon": 2.167939},
+                            mapbox_style="open-street-map", 
+                            color_continuous_scale='sunsetdark',
+                            zoom=10) 
+        fig1.update_layout(margin={"r":0,"t":0,"l":0,"b":0})   
+
+        #Serious
+        accident_cnt_per_district_serious = df_filter.groupby(by="District_Name").sum()[["Serious_injuries", "Victims", "Vehicles_involved"]]\
+                                    .rename(columns={"Serious_injuries":"Count_Serious_injuries"})
+        accident_avg_lat_lon = df_filter.groupby(by="District_Name").mean()[["Longitude", "Latitude"]]
+
+        accident_cnt_per_district_serious = accident_cnt_per_district_serious.join(accident_avg_lat_lon)
+        accident_cnt_per_district_serious = accident_cnt_per_district_serious.reset_index()
+
+        ## main plots
+        fig2 = px.choropleth_mapbox(accident_cnt_per_district_serious, 
+                            geojson=Barcelona_data, 
+                            color="Count_Serious_injuries",
+                            locations="District_Name", 
+                            featureidkey="properties.NOM",
+                            center={"lat": 41.389223, "lon": 2.167939},
+                            mapbox_style="open-street-map", 
+                            color_continuous_scale='sunsetdark',
+                            zoom=10)
+        ## set background color
+        fig2.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+        panel = html.Div([
+            html.H4(district),
+            dbc.Card(body=True, className="text-white bg-primary", children=[
+                
+                html.H6("Total Victims:", style={"color":"white"}),
+                html.H3("{:,.0f}".format(accident_cnt_per_district_mild['Victims'].sum()), style={"color":"white"}),
+
+                html.H6("Total Vehicles Involved:", className="text-danger"),
+                html.H3("{:,.0f}".format(accident_cnt_per_district_mild['Vehicles_involved'].sum()), className="text-danger")
+            ])
+        ])
+        
+    return fig1, fig2, panel
